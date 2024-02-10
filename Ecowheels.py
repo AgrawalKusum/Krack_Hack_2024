7f0943adfa32072ad301595e98b2de1c035fc121
AgrawalKusum <kusum.agrawal.0202@gmail.com>
2024-02-10 12:39:56 +0530

    create login interface

--- Ecowheels.py
+++ Ecowheels.py
@@ -8,12 +8,21 @@ from kivy.uix.stacklayout import StackLayout
 from kivy.uix.scrollview import ScrollView
 from kivy.metrics import dp
 from kivy.uix.pagelayout import PageLayout
+from kivy.lang import Builder
+from kivymd.app import MDApp
 from kivy.properties import StringProperty
 
+#for login page
 class EcoWheelWidgets(Widget):
     pass
 
-class EcoWheelsApp(App):
+class EcoWheelsApp(MDApp):
     def build(self):
-        return EcoWheelWidgets()
-EcoWheelsApp.run()
+        self.theme_cls.theme_style= "Dark"
+        self.theme_cls.primary_palette= "Cyan"
+        return Builder.load_file('login_page.kv')
+    def clear(self):
+        self.root.ids.user.text = ""
+        self.root.ids.password.text = ""
+
+EcoWheelsApp().run()
